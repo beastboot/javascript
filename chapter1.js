60fad58fe7ab562ad43755a32dbff6e2791201db
Roshan <roshankumar86788@gmail.com>
2023-03-31 23:41:24 +0530

    Initial commit

---
+++ chapter1.js
@@ -0,0 +1,14 @@
+let a="Harry"
+let b=6
+console.log(a + b)
+
+console.log(typeof(a+b))
+
+const a1={
+    name:"harry",
+    section:1,
+    isprincipal:false
+}
+a1['friends']="shubham"
+a1['name']="lovish"
+console.log(a1)
---
+++ chapter2.js
@@ -0,0 +1,8 @@
+let age = prompt("Enter your age")
+let a = Number.parseInt(age)
+if (a > 10 && a < 20) {
+  console.log("your age lies between 10 and 20");
+}
+else {
+  console.log("your age does not lies between 10 and 20");
+}
